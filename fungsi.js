0b9ba70ec660ceb667f025465365ba4602b9a68e
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-06-08 19:41:17 +0700

    menambahkan 2 file

--- fungsi.js
+++ fungsi.js
@@ -1,1 +1,8 @@
 console.log("fungsi pertama");
+
+const tambah = (a, b) => {
+    console.log("Fungsi tambah");
+    return a + b;
+}
+
+console.log(tambah(5, 10));
---
+++ test1.js
@@ -0,0 +1,1 @@
+console.log("ini adalah test1.js");
